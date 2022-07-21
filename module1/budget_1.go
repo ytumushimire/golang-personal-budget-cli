07cf6310a48b55c579764cba213b80494e1a94dd
Yves Tumushimire <ytumushimire@truststamp.net>
2022-07-21 12:09:02 +0200

    completed module 1

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,14 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	Max   float32
+	Items []Item
+}
 
 // Item stores item information
+
+type Item struct {
+	Description string
+	Price       float32
+}
